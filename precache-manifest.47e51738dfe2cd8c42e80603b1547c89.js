8e201872dfd36311eac71d9604c71a30009776b9
linbin <linbin@chandashi.com>
2019-12-05 17:00:08 +0800

    Updates

---
+++ precache-manifest.47e51738dfe2cd8c42e80603b1547c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe5d9e6c9580f10d81ba",
+    "url": "/easyokr.github.io/static/css/main.4d794e76.chunk.css"
+  },
+  {
+    "revision": "fe5d9e6c9580f10d81ba",
+    "url": "/easyokr.github.io/static/js/main.fe5d9e6c.chunk.js"
+  },
+  {
+    "revision": "dd96cba31a579f403150",
+    "url": "/easyokr.github.io/static/js/runtime~main.dd96cba3.js"
+  },
+  {
+    "revision": "1f6a5d0e886fa4fa62f3",
+    "url": "/easyokr.github.io/static/css/2.127af001.chunk.css"
+  },
+  {
+    "revision": "1f6a5d0e886fa4fa62f3",
+    "url": "/easyokr.github.io/static/js/2.1f6a5d0e.chunk.js"
+  },
+  {
+    "revision": "0d98e3465d3c921a1a3396fd4419d495",
+    "url": "/easyokr.github.io/index.html"
+  }
+];
